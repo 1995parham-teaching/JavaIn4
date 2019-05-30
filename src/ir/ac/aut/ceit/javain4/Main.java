91390bc4f5c700cb3e0865d57f5554d265d28622
Parham Alvani <parham.alvani@gmail.com>
2019-05-30 15:04:06 +0430

    feat: Update the Parsing Section in Main

--- src/ir/ac/aut/ceit/javain4/Main.java
+++ src/ir/ac/aut/ceit/javain4/Main.java
@@ -1,5 +1,7 @@
 package ir.ac.aut.ceit.javain4;
 
+import ir.ac.aut.ceit.javain4.calculator.Calculator;
+import ir.ac.aut.ceit.javain4.calculator.RemoteCalculator;
 import ir.ac.aut.ceit.javain4.server.Server;
 
 import java.io.IOException;
@@ -18,12 +20,21 @@ public class Main {
         }
         new Thread(srv).start();
 
+        Calculator calculator;
+        try {
+            calculator = new RemoteCalculator("127.0.0.1", 1373);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Distributed Calculator");
         boolean isRun = true;
         int result;
         while (isRun) {
             result = 0;
+
             System.out.print("> ");
 
             if (scanner.hasNext("quit")) {
@@ -32,34 +43,34 @@ public class Main {
             }
 
             String input = scanner.nextLine();
-            int sign = 1;
-            int num = 0;
-            String tmp = "";
-            for(int i=0;i<input.length();i++)
-            {
-                if(input.charAt(i) == '+') {
-                    num = Integer.parseInt(tmp);
-                    tmp = "";
-                    result = result + sign * num;
+            int sign = 1; // stores the last sign
+            StringBuilder tmp = new StringBuilder(); // builds the string that is used as the last read number
+            int num;
+            for (int i = 0; i < input.length(); i++) {
+                if (input.charAt(i) == '+') {
+                    num = Integer.parseInt(tmp.toString());
+                    tmp = new StringBuilder();
+                    result = calculator.add(result, sign * num);
                     sign = 1;
-                }
-                else if(input.charAt(i) == '-') {
-                    num = Integer.parseInt(tmp);
-                    tmp = "";
-                    result = result + sign * num;
+                } else if(input.charAt(i) == '-') {
+                    num = Integer.parseInt(tmp.toString());
+                    tmp = new StringBuilder();
+                    result = calculator.add(result, sign * num);
                     sign = -1;
+                } else {
+                    tmp.append(input.charAt(i));
                 }
-                else
-                    tmp+=input.charAt(i);
             }
-            num = Integer.parseInt(tmp);
-            result = result + sign * num;
+            num = Integer.parseInt(tmp.toString());
+            result = calculator.add(result, sign * num);
+
             System.out.println(result);
         }
 
         System.out.println("Hope you enjoy");
         try {
             srv.stop();
+            calculator.quit();
         } catch (IOException e) {
             e.printStackTrace();
         }
